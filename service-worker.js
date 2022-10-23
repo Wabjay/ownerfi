3416d6e06e360532f20bc62dc94539a49ec770db
Abdulwahab O. Jimoh <omoniyi.success@gmail.com>
2022-10-24 00:19:42 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ownerfi/precache-manifest.93d2cd8219432248620800f73a725f04.js"
+  "/ownerfi/precache-manifest.328b8a021930a1127f21ed23a35553f5.js"
 );
 
 workbox.clientsClaim();
